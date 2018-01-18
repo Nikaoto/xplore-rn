957f9869e967f5bfd2d374a4b2af0cf57d3db1b7
Nika Otiashvili <nikaoto@gmail.com>
2018-01-19 00:40:18 +0400

    add app index.js for exporting all func

--- app/index.js
+++ app/index.js
@@ -1,24 +1,33 @@
 import React from "react"
+import Welcome from "./components/Welcome"
+import BottomNavigation from "./components/BottomNavigation"
+import SearchScreen from "./components/SearchScreen"
 import {
   Text,
   View,
   StyleSheet,
 } from "react-native"
-import Welcome from "./components/Welcome"
+import { StackNavigator } from "react-navigation"
 
-export default class XploreApp extends React.Component {
+const XploreApp = StackNavigator({
+  Home: {
+    screen: BottomNavigation,
+    navigationOptions: {
+      title: "Xplore",
+    },
+  },
+})
+
+export default XploreApp
+
+/*export default class XploreApp extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Welcome/>
-        <Text>Open up App.js toooo start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <BottomNavigation />
     );
   }
 }
-
+*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -27,3 +36,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+/*
+<View>
+        <View style={styles.container}>
+          <Welcome/>
+          <Text>Open up App.js toooo start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+
+      </View>*/
