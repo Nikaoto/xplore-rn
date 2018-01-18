d9dc4d9140c1c40fe713657b38591fe524f53da2
Nika Otiashvili <nikaoto@gmail.com>
2018-01-19 00:41:05 +0400

    add SearchScreen comp

---
+++ app/components/SearchScreen/SearchScreen.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Text } from "react-native"
+
+export default class SearchScreen extends React.Component {
+  render() {
+    return <Text>SearchScreen class text</Text>
+  }
+}
