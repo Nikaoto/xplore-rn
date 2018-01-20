38b0782f1dde74c0f32dae739588bee393790d47
Nika Otiashvili <nikaoto@gmail.com>
2018-01-21 00:41:11 +0400

    clean up app/index.js

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Welcome from "./components/Welcome"
 import BottomNavigation from "./components/BottomNavigation"
-import SearchScreen from "./components/SearchScreen"
 import {
   Text,
   View,
@@ -19,31 +18,3 @@ const XploreApp = StackNavigator({
 })
 
 export default XploreApp
-
-/*export default class XploreApp extends React.Component {
-  render() {
-    return (
-      <BottomNavigation />
-    );
-  }
-}
-*/
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-/*
-<View>
-        <View style={styles.container}>
-          <Welcome/>
-          <Text>Open up App.js toooo start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-        </View>
-
-      </View>*/
