0a7ebcbf07ecd93186c5aa2229d8f355805a401d
Nika Otiashvili <nikaoto@gmail.com>
2018-01-21 14:32:30 +0400

    add values; add values/colors.js

---
+++ app/values/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  primary: "#48c201",
+  primaryDark: "#309f4c",
+  accent: "#46d196",
+  dialect: "#ff4051",
+  subtleGreen: "#00e64d",
+  subtleRed: "#ff2d1a",
+}
+
+export default colors
