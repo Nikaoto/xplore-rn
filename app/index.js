b4e1d3351d3afae3c67cf72030db0065886b1772
Nika Otiashvili <nikaoto@gmail.com>
2018-01-22 22:06:46 +0400

    fix userLoggedIn always false

--- app/index.js
+++ app/index.js
@@ -12,7 +12,7 @@ class HomeScreen extends Component {
   constructor(props) {
     super(props)
     this.state = {userLoggedIn: null}
-    isUserLoggedIn().then(userLoggedIn => this.setState({userLoggedIn: false}))
+    isUserLoggedIn().then(userLoggedIn => this.setState({userLoggedIn: userLoggedIn}))
   }
 
   static navigationOptions = {
