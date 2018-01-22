c44ab6a2ddd7f3b9360508f5aff6a8ccb9838ca9
Nika Otiashvili <nikaoto@gmail.com>
2018-01-22 21:12:38 +0400

    add user logged in asyncStorage modifiers to AccountUtil

--- app/reducers/AccountUtil.js
+++ app/reducers/AccountUtil.js
@@ -4,15 +4,24 @@ const USER_LOGGED_IN_KEY = "@Xplore:user_logged_in"
 
 export async function isUserLoggedIn() {
   try {
-    const value = await AsyncStorage.getItem(USER_LOGGED_IN_KEY)
+    console.log("isUserLoggedIn()")
+    const value = await AsyncStorage.getItem(USER_LOGGED_IN_KEY, (value) => JSON.parse(value))
     if (value !== null) {
-      console.log("we have a value bois")
       return value
     } else {
+      console.log("value === null")
+      setUserLoggedIn(false)
       return false
     }
   } catch (error) {
     console.log(error)
-    return false
+  }
+}
+
+export async function setUserLoggedIn(loggedIn) {
+  try {
+    return await AsyncStorage.setItem(USER_LOGGED_IN_KEY, JSON.stringify(loggedIn))
+  } catch (error) {
+    console.log(error)
   }
 }
