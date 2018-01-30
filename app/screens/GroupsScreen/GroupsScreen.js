ecad34d5c5599c243c83fe9823018127438bc5bd
Nika Otiashvili <nikaoto@gmail.com>
2018-01-31 01:47:02 +0400

    add groupcards in GroupsScreen

--- app/screens/GroupsScreen/GroupsScreen.js
+++ app/screens/GroupsScreen/GroupsScreen.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {Image, StyleSheet, Text, View} from "react-native"
 import globalStyles from "../../values/styles"
+import GroupCard from "../../components/GroupCard/GroupCard"
 
 export default class GroupsScreen extends React.Component {
 
@@ -17,6 +18,24 @@ export default class GroupsScreen extends React.Component {
     return (
       <View style={globalStyles.container}>
         <Text style={styles.titleText}>Groups Screen</Text>
+        <GroupCard
+          leaderId={"qwe"}
+          groupId={"asd"}
+          groupName={"Excursion in Javakheti"}
+          memberCount={7}
+          durationInDays={3}
+          startsInDays={5}
+          experienced={false}
+        />
+        <GroupCard
+          leaderId={"qwe"}
+          groupId={"asd"}
+          groupName={"Excursion in Javakheti"}
+          memberCount={7}
+          durationInDays={3}
+          startsInDays={5}
+          experienced={false}
+        />
       </View>
     )
   }
