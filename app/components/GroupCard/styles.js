4cd57fd28ba787646bd89d1a7126e9e54cff48d5
Nika Otiashvili <nikaoto@gmail.com>
2018-02-02 00:21:57 +0400

    add external styles to GroupCard

---
+++ app/components/GroupCard/styles.js
@@ -0,0 +1,89 @@
+import {Platform, StyleSheet} from "react-native"
+import colors from "../../values/colors"
+
+const mediumFont = Platform.OS === "ios" ? "normal" : "sans-serif-medium"
+
+const styles = StyleSheet.create({
+  topLayout: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  groupCard: {
+    marginTop: 15,
+    marginHorizontal: 5,
+    padding: 0,
+    alignSelf: "stretch",
+  },
+  leaderLayout: {
+    margin: 10,
+    flexDirection: "row",
+  },
+  leaderImage: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+  },
+  leaderTextLayout: {
+    marginLeft: 5,
+  },
+  leaderReputationText: {
+    color: colors.greyText,
+    fontFamily: mediumFont,
+    fontSize: 13,
+  },
+  leaderNameText: {
+    color: colors.greyestText,
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  badgeLayout: {
+    margin: 3,
+    display: "flex",
+    flexDirection: "row-reverse",
+  },
+  beenHereIcon: {
+    marginLeft: 3,
+    width: 20,
+    height: 24,
+  },
+  inviteIcon: {
+    width: 20,
+    height: 22,
+  },
+  groupImage: {
+    height: 85,
+    width: "auto",
+  },
+  groupNameText: {
+    marginHorizontal: 10,
+    marginVertical: 3,
+    color: colors.greyestText,
+    fontWeight: "bold",
+  },
+  divider: {
+    borderBottomColor: 'lightgrey',
+    borderBottomWidth: 1,
+  },
+  footer: {
+    marginTop: 2,
+    marginBottom: 2,
+    marginHorizontal: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  footerSection: {
+    flexDirection: "row",
+    marginHorizontal: 5,
+  },
+  footerIcon: {
+    height: 22,
+    width: 22,
+  },
+  footerText: {
+    color: colors.greyerText,
+    marginLeft: 3,
+    paddingTop: 1,
+  },
+})
+
+export default styles
